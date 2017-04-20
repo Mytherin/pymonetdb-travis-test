ddfcf1feb0194555a48322b9313febeb9fc334fe
Mark Raasveldt <mark.raasveldt@gmail.com>
2017-04-20 13:08:27 +0200

    For travis compile MonetDB from default branch instead of installing from the repo so the latest protocol is used..

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,16 +1,27 @@
 #!/bin/bash -ve
 
 # install monetdb
-sudo apt-get install software-properties-common
-sudo apt-get update -q
-sudo sh -c "echo 'deb http://dev.monetdb.org/downloads/deb/ precise monetdb' > /etc/apt/sources.list.d/monetdb.list"
-wget --output-document=- http://dev.monetdb.org/downloads/MonetDB-GPG-KEY | sudo apt-key add -
-sudo apt-get update -q
-sudo apt-get install -qy monetdb5-sql monetdb-client
+sudo apt-get install software-properties-common libpcre3-dev libxml2-dev autoconf pkgconf libssl-dev
 
-# start database
-sudo sh -c "echo 'STARTUP=yes\nDBFARM=/var/lib/monetdb\n' > /etc/default/monetdb5-sql"
-sudo service monetdb5-sql start
+wget https://dev.monetdb.org/hg/MonetDB/archive/default.tar.gz
+tar xvf default.tar.gz
+cd MonetDB-default
+./bootstrap
+./configure --enable-debug --enable-assert --disable-optimize
+make -j
+sudo make install
+
+
+# sudo apt-get install software-properties-common
+# sudo apt-get update -q
+# sudo sh -c "echo 'deb http://dev.monetdb.org/downloads/deb/ precise monetdb' > /etc/apt/sources.list.d/monetdb.list"
+# wget --output-document=- http://dev.monetdb.org/downloads/MonetDB-GPG-KEY | sudo apt-key add -
+# sudo apt-get update -q
+# sudo apt-get install -qy monetdb5-sql monetdb-client
+
+# # start database
+# sudo sh -c "echo 'STARTUP=yes\nDBFARM=/var/lib/monetdb\n' > /etc/default/monetdb5-sql"
+# sudo service monetdb5-sql start
 
 # set up test database
 sudo monetdb create demo
